0fc5616908290e587dc1fe6ea52d0282f22bc83b
Konst Kolesnichenko <const@dynamic-systems.com.ua>
2016-02-02 19:39:04 +0200

    * added more stun servers

--- settings.py
+++ settings.py
@@ -6,7 +6,9 @@ BIND_PORT = 3333
 WEBRTC_LISTENER = "http://127.0.0.1:8000/api/webrtc/"
 
 ICE_SERVERS = [
-    "stun:104.45.22.14:3478"
+    "stun:104.45.22.14:3478",
+    "stun:stun.l.google.com:19302",
+    "stun:173.255.206.76:3478"
 ]
 
 try:
