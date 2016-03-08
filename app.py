ba17daf7aee49687e4e357bd526de29f7f82e220
Konst Kolesnichenko <kolesnichenko@gmail.com>
2016-03-08 22:57:44 +0200

    * weird hack reverted

--- app.py
+++ app.py
@@ -92,8 +92,7 @@ def process_message(data, client):
 
 @asyncio.coroutine
 def handle_incoming_packet(client, data):
-    # weird hack, because some browsers sometimes are doing double-json conversion of a payload
-    packet = json.loads(json.loads(data))
+    packet = json.loads(data)
     if packet.get('type') is None:
         return_error(client, "unknown message: " + data, ERR_UNKNOWN_MESSAGE)
         return
